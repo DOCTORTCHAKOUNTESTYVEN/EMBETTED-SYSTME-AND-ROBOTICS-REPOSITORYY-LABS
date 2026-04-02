c2f8b6dd9c95b3a1523d3d3317d6e63858ea6f5f
DOCTOR-STYVEN <tnstivo1@gmail.com>
2026-04-02 09:45:40 +0100

    Lab 1.1: Hello World in C and C++

--- hello.c
+++ hello.c
@@ -3,5 +3,8 @@
 int main(void) {
     printf("Hello from C!\n");
     printf("This program has %lu bytes in an int.\n", sizeof(int));
+    
+    printf("\nAppuyez sur Entree pour quitter...");
+    getchar(); // Cette ligne attend une saisie avant de finir [cite: 258]
     return 0;
 }
--- hello.cpp
+++ hello.cpp
@@ -4,5 +4,8 @@ using namespace std;
 int main() {
     cout << "Hello from C++!" << endl;
     cout << "This program has " << sizeof(int) << " bytes in an int." << endl;
+    
+    cout << "\nAppuyez sur Entree pour quitter...";
+    cin.get(); // Equivalent en C++ [cite: 184]
     return 0;
 }
